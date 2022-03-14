8fd6aa3255c79bb298a4085ca114b99e4da88a25
hakohumi <mihumihu32@gmail.com>
2022-03-14 19:56:10 +0900

    途中

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,27 +3,42 @@
   <button @click="increment">Increment</button>
   <button @click="decrement">Decrement</button>
 
-  <div class="input-area">
-    <input v-model="gyoumu_title" placeholder="業務名" />
-    {{ gyoumu_title }}
+  <div class="task-template">
+    <div style="display: flex; flex-direction: row">
+      <input v-model="gyoumu_title" placeholder="業務名" />
+      <input
+        v-model="view_task_time"
+        placeholder="作業時間"
+        v-on:change="on_change_task_time"
+      />
+      {{ task_rate }}%
+    </div>
     <textarea v-model="message" placeholder="メモ"></textarea>
-    {{ message }}
   </div>
 </template>
 
 <style>
-.input-area {
+.task-template {
   display: flex;
   flex-direction: column;
 }
 </style>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const count = ref(0)
 
-const gyoumu_title = ref<string>('test')
+const gyoumu_title = ref<string>('')
+const task_time = ref('')
+let view_task_time: string = ''
+let editing_task_time: string = ''
+const task_rate = ref(0)
+
+const on_change_task_time = () => {
+  view_task_time = view_task_time + 'h'
+}
+
 const message = ref<string>('')
 
 const increment = () => {
