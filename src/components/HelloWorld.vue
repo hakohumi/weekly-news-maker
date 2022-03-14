078c10a93d042b1689cc00e64f6bf73d50187afa
hakohumi <mihumihu32@gmail.com>
2022-03-14 19:28:36 +0900

    Composition API 書き方練習

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,33 @@
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+
+  <div class="input-area">
+    {{ message }}
+    <textarea v-model="message" placeholder="add multiple lines"></textarea>
+  </div>
+</template>
+
+<style>
+.input-area {
+  display: flex;
+  flex-direction: column;
+}
+</style>
+
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
 const count = ref(0)
+
+const message = ref<string>(' ')
+
+const increment = () => {
+  count.value++
+}
+
+const decrement = () => {
+  count.value--
+}
 </script>
-
-<template>
-  <h1>{{ msg }}</h1>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-
-</template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
