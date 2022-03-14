7ff5b5eb5f77af974f11710edce784881a8d0976
hakohumi <mihumihu32@gmail.com>
2022-03-14 19:28:08 +0900

    setupではない前の書き方の保存

---
+++ src/components/HelloWorld_old.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const count = ref(0);
+
+    const increment = () => {
+      count.value++;
+    };
+
+    const decrement = () => {
+      count.value--;
+    };
+    return {
+      count,
+      increment,
+      decrement,
+    };
+  },
+});
+</script>
